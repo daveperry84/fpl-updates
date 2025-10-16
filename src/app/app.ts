833509fdbda653042ad237e753c4f1f54b39a42e
Dave P <daveperry.dev@gmail.com>
2025-10-16 17:32:11 +0100

    Move header into its own component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Footer } from "./footer/components/footer/footer";
+import { Header } from './header/components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, Footer],
+  imports: [RouterOutlet, RouterModule, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
