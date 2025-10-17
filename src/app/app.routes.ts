9c669b2257e050ad5b00acc78bd4422c4f4c92d5
Dave P <daveperry.dev@gmail.com>
2025-10-17 18:14:45 +0100

    revert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./home/components/home/home').then(m => m.Home)
+        redirectTo: '/home',
+        pathMatch: 'full'
     },
     {
         path: 'home',
