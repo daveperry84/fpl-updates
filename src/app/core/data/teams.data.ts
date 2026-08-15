ec03a4f427455cacd99a55d60578dc69bdda90fd
Dave P <daveperry.dev@gmail.com>
2026-08-15 19:20:33 +0100

    Add new teams

--- src/app/core/data/teams.data.ts
+++ src/app/core/data/teams.data.ts
@@ -15,4 +15,10 @@ export const TEAMS: ReadonlyArray<Team> = [
     { id: 7, name: "Gayfield of Dreams", manager: "Simon Lavery" },
     { id: 8, name: "Delap of the Gods", manager: "Sandeep Dhillon" },
     { id: 9, name: "Redder's Rejects", manager: "David Redfern" },
+    { id: 10, name: "Dragospear's XI", manager: "Areeb Dar" },
+    { id: 11, name: "Richmond FC", manager: "Matthew Doyle", isPaid: true },
+    { id: 12, name: "DuncanDisorder", manager: "Liam Duncan" },
+    { id: 13, name: "Not too shabbyAlonso", manager: "Tom Wyer" },
+    { id: 14, name: "Sharonaldo", manager: "Sharon Perry", isPaid: true },
+    { id: 15, name: "Kinder Mbeumo", manager: "Alex Burney" },
 ];
